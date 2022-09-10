3ef9a59d563d764a29fd83dea343f21d3162c664
aidi <aidi3181503023@gmail.com>
2022-09-10 21:14:36 +0800

    去结算页面 底部

---
+++ .history/src/store/index_20220906215932.js
@@ -0,0 +1,101 @@
+import Vuex from 'vuex'
+
+const setLocalCartList = (state) => {
+  const { cartList } = state
+  const cartListString = JSON.stringify(cartList)
+  localStorage.cartList = cartListString
+}
+
+const getLocalCartList = () => {
+  // { shopId: {shopName:'', productList:{ productId: {} }}}
+  if (typeof localStorage.cartList !== 'undefined') { // 避免直接使用JSON.parse 引起“JSON.parse(<anonymous>)”
+    const JSONCartList = JSON.parse(localStorage.cartList)
+    return JSONCartList
+  }
+  return {}
+}
+
+export default Vuex.createStore({
+  state: {
+    // { shopId: {shopName: '' , productList: { productId: {} } }}
+    cartList: getLocalCartList()
+    // 一开始购物车是没有内容的 以下内容仅是为了展示层级解构
+    // // 第一层级是商铺的id(不同的商铺添加到购物车)
+    // shopId: {
+    //   // 第二层是商品id(同一店铺下 不同的商品)
+    //   // 第二层内容是商品内容以及购物数量
+    //   shopName: '',
+    //   productList: {
+    //    productId: {
+    //     _id: '1',
+    //     name: '番茄250g/份',
+    //     imgUrl: 'http://www.dell-lee.com/imgs/vue3/tomato.png',
+    //     sales: 10,
+    //     price: 33.6,
+    //     oldPrice: 39.6,
+    //     count: 2,//后端返回时是没有的 前端加上
+    //     check: true
+    //    },
+    //   }
+    // },
+  },
+  mutations: {
+    changeCartItemInfo (state, payload) {
+      const { shopId, productId, productInfo } = payload
+      const shopInfo = state.cartList[shopId] || {
+        shopName: '',
+        productList: {}
+      }
+      let product = shopInfo.productList[productId]
+      if (!product) {
+        product = productInfo
+        product.count = 0
+      }
+      product.count = product.count + payload.num
+      if (payload.num > 0) {
+        product.check = true
+      }
+      if (product.count < 0) {
+        product.count = 0
+      }
+      shopInfo.productList[productId] = product
+      state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
+    },
+    changeCartItemChecked (state, payload) {
+      const { shopId, productId } = payload
+      const product = state.cartList[shopId].productList[productId]
+      product.check = !product.check
+      setLocalCartList(state)
+    },
+    clearCartProducts (state, payload) {
+      const { shopId } = payload
+      state.cartList[shopId].productList = {}
+      setLocalCartList(state)
+    },
+    setCartItemsChecked (state, payload) {
+      const { shopId } = payload
+      const products = state.cartList[shopId].productList
+      if (products) {
+        for (const i in products) {
+          const product = products[i]
+          product.check = true
+        }
+      }
+      setLocalCartList(state)
+    },
+    changeShopName (state, payload) {
+      const { shopId, shopName } = payload
+      // console.log(shopName)
+      const shopInfo = state.cartList[shopId] || {
+        shopName: '',
+        productList: {}
+      }
+      shopInfo.shopName = shopName // shopInfo.shopName 而不是 
+      state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
+    }
+  },
+  actions: {},
+  modules: {}
+})
---
+++ .history/src/views/shop/Shop_20220906211813.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <div class="search__back iconfont" @click="handleBackClick">&#xe697;</div>
+      <div class="search__content">
+        <span class="search__content__icon iconfont">&#xe604;</span>
+        <input class="search__content__input" placeholder="请输入商品名称" />
+      </div>
+    </div>
+    <ShopInfo :item="item" />
+    <Content :shopName/>
+    <Cart/>
+  </div>
+</template>
+<script>
+import { reactive, toRefs } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import ShopInfo from '@/components/ShopInfo.vue'
+import { get } from '@/utils/request'
+import Content from '@/views/shop/Content.vue'
+import Cart from './Cart'
+// 获取当前商铺信息
+const useShopInfoEffect = () => {
+  const route = useRoute() // 包含当前路由的一些信息（比如URL里的id）
+  const data = reactive({
+    item: {}
+  })
+  const getItemData = async () => {
+    // console.log(route.name,route.params)
+    // console.log(route.params.id) (为什么刚刚好是id是因为 router里path就是'/shop/:id'而不是 '/shop/:aiid'')点击不同的商家携带的id也不同
+    const result = await get(`/api/shop/${route.params.id}`) // 根据route的id的不同请求不同的后端接口
+    // console.log(result)
+    if (result?.errno === 0 && result?.data) {
+      data.item = result.data
+    }
+  }
+  const { item } = toRefs(data)
+  return { item, getItemData }
+}
+
+// 点击回退的逻辑
+const useBackRouterEffect = () => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    router.back()
+  }
+  return { handleBackClick }
+}
+export default {
+  name: 'Shop',
+  components: { ShopInfo, Content, Cart },
+  setup () {
+    // 1.获取当前商铺信息并立刻执行
+    // 2.处理回退按钮的逻辑
+    const { item, getItemData } = useShopInfoEffect()
+    const { handleBackClick } = useBackRouterEffect()
+    getItemData()
+    return { item, getItemData, handleBackClick }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  padding: 0 0.18rem;
+}
+.search {
+  display: flex;
+  flex: 1;
+  margin: 0.2rem 0 0.16rem 0;
+  line-height: 0.32rem;
+  &__back {
+    width: 0.3rem;
+    font-size: 0.24rem;
+    color: #b6b6b6;
+  }
+  &__content {
+    display: flex;
+    flex: 1;
+    background: #f5f5f5;
+    border-radius: 0.16rem;
+    &__icon {
+      text-align: center;
+      width: 0.44rem;
+      color: #b7b7b7;
+    }
+    &__input {
+      width: 100%;
+      border: none;
+      outline: none;
+      background: none;
+      padding-top: 0.05rem;
+      font-size: 0.14rem;
+      &::placeholder {
+        color: #333;
+      }
+    }
+  }
+}
+</style>
