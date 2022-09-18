5be19ff3ee95e911ffe2b6144573f33fad906a2c
aidi <aidi3181503023@gmail.com>
2022-09-18 21:53:53 +0800

    提交订单 与 取消订单

---
+++ .history/src/effects/commonCartEffect_20220918215113.js
@@ -0,0 +1,49 @@
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+// 购物车相关逻辑
+export const useCommonCartEffect = (shopId) => {
+  const store = useStore()
+  const cartList = store.state.cartList
+  const changeCartItemInfo = (shopId, productId, productInfo, num) => {
+    store.commit('changeCartItemInfo', {
+      shopId,
+      productId,
+      productInfo,
+      num
+    })
+  }
+  const productList = computed(() => {
+    const productList = cartList[shopId]?.productList || {}
+    const notEmptyProductList = {}
+    for (const i in productList){
+      const product = productList[i]
+      if (product.count > 0){
+        
+      }
+    }
+    return productList
+  })
+  const shopName = computed(() => {
+    const shopName = cartList[shopId]?.shopName || ''
+    return shopName
+  })
+  const calculations = computed(() => {
+    const productList = cartList[shopId]?.productList
+    const result = { total: 0, price: 0, allChecked: true }
+    if (productList) {
+      for (const i in productList) {
+        const product = productList[i]
+        result.total += product.count
+        if (product.check) {
+          result.price += product.count * product.price // 包装了一层result后 使用price变成了 result.price 没有单个price了
+        }
+        if (product.count > 0 && !product.check) {
+          result.allChecked = false
+        }
+      }
+    }
+    result.price = result.price.toFixed(2)
+    return result
+  })
+  return { changeCartItemInfo, cartList, productList, shopName, calculations }
+}
---
+++ .history/src/views/orderConfirmation/Order_20220918213322.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="order">
+    <div class="order__price">
+      实付金额 <b>¥{{ calculations.price }}</b>
+    </div>
+    <div class="order__btn" @click="handleShowConfirmChange(true)">
+      提交订单
+    </div>
+  </div>
+  <div class="mask" v-show="showConfirm" @click="handleShowConfirmChange(false)">
+    <div class="mask__content" @click.stop>
+      <h3 class="mask__content__title">确认要离开收银台？</h3>
+      <p class="mask__content__desc">请尽快完成支付，否则将被取消</p>
+      <div class="mask__content__btns">
+        <div
+          class="mask__content__btn mask__content__btn--first"
+          @click="() handleCancelOrder"
+        >
+          取消订单
+        </div>
+        <div
+          class="mask__content__btn mask__content__btn--last"
+          @click="() => handleConfirmOrder(false)"
+        >
+          确认支付
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { useCommonCartEffect } from '@/effects/commonCartEffect'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+// 蒙层相关逻辑
+const useShowMaskEffect = () => {
+  const showConfirm = ref(false)
+  const handleShowConfirmChange = (status) => {
+    showConfirm.value = status
+  }
+  return { showConfirm, handleShowConfirmChange }
+}
+
+// // 下单相关逻辑
+// const useMakeOrderEffect = () => {
+
+// }
+export default {
+  name: 'Order',
+  setup () {
+    const route = useRoute()
+    const shopId = parseInt(route.params.id, 10)
+    const { calculations, productList } = useCommonCartEffect(shopId)
+    const { showConfirm, handleShowConfirmChange } = useShowMaskEffect()
+    const handleConfirmOrder = async (isCanceled) => {
+      const products = []
+      for (const i in productList.value) {
+        const product = productList.value[i]
+        products.push({ id: parseInt(), num: product.count })
+      }
+      console.log(products)
+    }
+    return {
+      showConfirm,
+      handleShowConfirmChange,
+      calculations,
+      handleConfirmOrder
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "@/style/variables.scss";
+.order {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  height: 0.49rem;
+  line-height: 0.49rem;
+  background: $bgColor;
+  &__price {
+    flex: 1;
+    text-indent: 0.24rem;
+    font-size: 0.14rem;
+    color: $content-fontcolor;
+  }
+  &__btn {
+    width: 0.98rem;
+    background: #4fb0f9;
+    color: #fff;
+    text-align: center;
+    font-size: 0.14rem;
+  }
+}
+.mask {
+  z-index: 1;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+  &__content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 3rem;
+    height: 1.56rem;
+    background: #fff;
+    text-align: center;
+    border-radius: 0.04rem;
+    &__title {
+      margin: 0.24rem 0 0 0;
+      line-height: 0.26rem;
+      font-size: 0.18rem;
+      color: #333;
+    }
+    &__desc {
+      margin: 0.08rem 0 0 0;
+      font-size: 0.14rem;
+      color: #666666;
+    }
+    &__btns {
+      display: flex;
+      margin: 0.24rem 0.58rem;
+    }
+    &__btn {
+      flex: 1;
+      width: 0.8rem;
+      line-height: 0.32rem;
+      border-radius: 0.16rem;
+      font-size: 0.14rem;
+      &--first {
+        margin-right: 0.12rem;
+        border: 0.01rem solid #4fb0f9;
+        color: #4fb0f9;
+      }
+      &--last {
+        margin-left: 0.12rem;
+        background: #4fb0f9;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
